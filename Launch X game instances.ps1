92fcd43b49186074d5de8ae86309a66505632c65
Jorge Luque <guitarjorge24@outlook.com>
2021-08-21 20:13:49 -0400

    - Added powershell script to launch x number of game instances

---
+++ Launch X game instances.ps1
@@ -0,0 +1,4 @@
+for ($i=1; $i -le $args[0]; $i++) 
+{
+	D:\Build\++UE4\Sync\Engine\Binaries\Win64\UE4Editor-Win64-DebugGame.exe C:\Users\Jorge\Documents\UE\UMM\GoGoPowerKarts\GoGoPowerKarts.uproject -game -log -nosteam
+}
